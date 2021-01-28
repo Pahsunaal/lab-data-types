443ebcba31a658f76213540bcdbb532504f57a1d
Pahsunaal <timmitcheson@gmail.com>
2021-01-28 17:36:41 +0000

    Finished Exercises

--- src/Lab.hs
+++ src/Lab.hs
@@ -10,39 +10,32 @@ module Lab where
 data IntPos = IntPos Int Int 
 
 instance Eq IntPos where 
-    (IntPos x0 y0) == (IntPos x1 y1) = undefined 
+    (IntPos x0 y0) == (IntPos x1 y1) = (x0 == x1) && (y0 == y1)
 
 instance Show IntPos where 
-    show (IntPos x y) = undefined 
+    show (IntPos x y) = "IntPos " ++ show x ++ " " ++ show y
 
 zeroPos :: IntPos 
-zeroPos = undefined 
+zeroPos = IntPos 0 0
 
 x :: IntPos -> Int 
-x = undefined 
+x (IntPos x0 _) = x0
 
 y :: IntPos -> Int 
-y = undefined 
+y (IntPos _ y0) = y0
 
 --------------------------------------------------------------------------------
 
-data Pos a = Pos a a 
+data Pos a = Pos { left :: a, top :: a }
 
 instance Eq a => Eq (Pos a) where 
-    (Pos x0 y0) == (Pos x1 y1) = undefined 
+    (Pos x0 y0) == (Pos x1 y1) = (x0 == x1) && (y0 == y1)
 
 instance Show a => Show (Pos a) where 
-    show (Pos x y) = undefined
+    show (Pos x y) = "Pos " ++ show x ++ " " ++ show y
 
 zero :: Num a => Pos a 
-zero = undefined 
-
-left :: Pos a -> a 
-left = undefined 
-
-top :: Pos a -> a 
-top = undefined 
-
+zero = Pos 0 0
 --------------------------------------------------------------------------------
 
 data DocumentItem = ListItem (Int -> String)
@@ -54,7 +47,8 @@ doc =
     , ListItem (\n -> concat ["There. Are. ", show n, ". Items." ])
     ]
 
-render :: [DocumentItem] -> Int -> String 
-render = undefined
+render :: [DocumentItem] -> Int -> String
+render [] _ = ""
+render (ListItem x:xs) n = x n ++ "\n" ++ render xs (n+1)
 
 --------------------------------------------------------------------------------
